19b678251ec5fa567eeac1f9c54efb5f5e3c980d
Mohamed Yehia <eng.essa85@gmail.com>
2026-02-12 16:32:19 +0200

    modify email error

--- app/api/send-email/route.ts
+++ app/api/send-email/route.ts
@@ -1,9 +1,10 @@
 import { Resend } from 'resend';
 import { NextResponse } from 'next/server';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+
 
 export async function POST(request: Request) {
+    const resend = new Resend(process.env.RESEND_API_KEY);
     try {
         const { storeName, personName, whatsapp, orders } = await request.json();
 
